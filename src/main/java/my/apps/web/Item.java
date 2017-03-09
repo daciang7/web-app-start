6925cf5017966770ea06af26e8813bf1606bc438
Dacian <daciang7@gmail.com>
2017-03-09 18:44:27 +0200

    ShoppingList

---
+++ src/main/java/my/apps/web/Item.java
@@ -0,0 +1,17 @@
+package my.apps.web;
+
+
+public class Item {
+    private  String nume;
+    private String cantitate;
+    public Item(String nume,String cantitate){
+        this.nume=nume;
+        this.cantitate=cantitate;
+    }
+    public String getNume(){
+        return nume;
+    }
+    public String getCantitate(){
+        return cantitate;
+    }
+}
